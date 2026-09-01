9cb7169553abf7869050c807935315f9c2053021
TristanDlh <tristan.delahousse@epitech.eu>
2026-09-01 16:00:19 +0200

    [ADD] Front -> Initialise frontend

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <p>Welcome to Legacycy la famille</p>
+      </section>
+    </>
+  )
+}
+
+export default App
