81aaca971eb2cc1a30fe3f234aa0aafe687cbf41
Mathis ZUCCHERO <mathis.zucchero@epitech.eu>
2026-09-07 14:18:59 +0200

    fix(prettier): fix coding style

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 
-import LegacyApp from "./app/legacy/LegacyApp.jsx";
+import LegacyApp from './app/legacy/LegacyApp.jsx';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <LegacyApp />
-  </StrictMode>,
+  </StrictMode>
 );
