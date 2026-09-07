eccb2a13a1b66cea72e8d01c65e2b7e75ff05a2a
Mathis ZUCCHERO <mathis.zucchero@epitech.eu>
2026-09-07 13:46:44 +0200

    feat(legacy): import old todolist app.js to a new LegacyApp.jsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import App from "./app/App.tsx";
+import LegacyApp from "./app/legacy/LegacyApp.jsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <LegacyApp />
   </StrictMode>,
 );
