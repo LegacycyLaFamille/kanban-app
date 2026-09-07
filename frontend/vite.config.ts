09d17bb1a3000e11b62341582c22eeb953423cd4
Théo Fisch-Dewailly <theo.fisch-dewailly@epitech.eu>
2026-09-07 19:26:25 +0200

    fix(ci): format backend files, allow backend tests to pass with no tests yet

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,8 +7,8 @@ export default defineConfig({
 
   server: {
     proxy: {
-      '/items': {
-        target: 'http://localhost:3000',
+      "/items": {
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
     },
